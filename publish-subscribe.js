3f4d85e842dd85432ec228326c241e9039f3bb64
AtomskJD <atomskjd@gmail.com>
2016-02-15 21:21:57 +0500

    publish subscribe pattern Это проще если разобраться получше Нужно закончить эту главу поскорее

--- publish-subscribe.js
+++ publish-subscribe.js
@@ -69,7 +69,7 @@ var messageLogger = function ( topics, data ) {
 
 var subscription = pubsub.subscribe( "inbox/newMessage", messageLogger );
 
-pubusb.publish( "inbox/newMessage", "hello world" );
+pubsub.publish( "inbox/newMessage", "hello world" );
 
 pubsub.publish( "inbox/newMessage", ["test", "a", "b", "c"] );
 
@@ -82,3 +82,48 @@ pubsub.unsubscribe( subscription );
 
 
 pubsub.publish( "inbox/newMessage", "hello! are you still there?");
+
+
+
+// user interface notification
+// return the current local time
+getCurrentTime = function () {
+  var date = new Date(),
+      m = date.getMonth() +1,
+      d = date.getDate(),
+      y = date.getFullYear(),
+      t = date.toLocaleTimeString().toLowerCase();
+
+  return (m + "/" + d + "/" + y + " " + t);
+};
+
+// Add a new data row to grid
+function addGridRow ( data ) {
+  console.log("update grid component with: " + data);
+}
+
+function updateCounter( data ) {
+  console.log( "data last updated at: " + getCurrentTime() + " with " + data);
+}
+
+gridUpdate = function ( topic, data ) {
+  if ( data !== undefined ) {
+    addGridRow( data );
+    updateCounter( data );
+  }
+};
+
+var subscriber = pubsub.subscribe( "newDataAvailable", gridUpdate );
+
+
+pubsub.publish( "newDataAvailable", {
+  summary: "Apple made $5 billion",
+  identifier: "APPL",
+  stockPrice: 570.91
+});
+
+pubsub.publish( "newDataAvailable", {
+  summary: "Microsoft made $20 million",
+  identifier: "MSFT",
+  stockPrice: 30.85
+});
